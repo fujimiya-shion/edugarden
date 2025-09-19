a0044f39c26a28e002196ddcff2e737fea9dc0bf
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-09-19 22:43:43 +0700

    style: spacing of detail page

--- resources/views/pages/clients/page/detail.blade.php
+++ resources/views/pages/clients/page/detail.blade.php
@@ -4,7 +4,7 @@
     <div id="staticPage">
         <div class="container py-5">
             @if ($page->image)
-                <img src="{{ Storage::url($page->image) }}" alt="{{ $page->title }}" width="100%" style="aspect-ratio: 21/9;">
+                <img src="{{ Storage::url($page->image) }}" alt="{{ $page->title }}" width="100%" style="aspect-ratio: 21/9;" class="rounded rounded-3">
             @endif
             <h2 class="text-color fw-600 @if ($page->image) mt-3 @endif">{{ $page->title }}</h2>
             <div class="mt-3">
